b2cf0c3a500a537fcaec1a6f0317088a8564da3c
Sandu Ioana <ioanasandu99@gmail.com>
2019-10-15 00:11:58 +0300

    Assignment1

---
+++ Students/Sandu Ioana/_pasted_code_/src/snippet/Snippet.java
@@ -0,0 +1,8 @@
+package snippet;
+
+public class Snippet {
+	public static void main(String[] args) {
+		if(str_Sample.length())
+	}
+}
+
---
+++ Students/Sandu Ioana/second-lab/src/HotelManager.java
@@ -0,0 +1,21 @@
+public class HotelManager {
+
+	public static void main(final String[] args) {
+		System.out.println("No input"); 
+	}	
+	/**
+	 * If on second floor then not clean (e.g. 200)
+	 * If on any other floor and even number then clean
+	 * Not clean otherwise
+	 * 
+	 * @param roomNumber XAA (X - floor number, AA - 0->19 room number)
+	 *                   < 0 && > 920 .. 655 invalid room number
+	 *                   0->19 is valid room number
+	 * @return
+	 */
+	public static boolean isClean(int roomNumber) {
+		public static final int FIRST_ROOM = 0;
+		public static final int LAST_ROOM = 920;
+		return false;
+	}
+}
